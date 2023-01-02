207ef2c34d4805efbefb2e2c0d361a0a74c6aa4f
yun <austru00@gmail.com>
2023-01-02 17:01:02 +0900

    230102

---
+++ 2python/실습1.py
@@ -0,0 +1,52 @@
+num = int(input("숫자를 입력해주세요"))
+print(num+10)
+
+food = input("좋아하는 음식을 입력해주세요")
+print("좋아하는 음식:", food)
+
+name=input("이름을 입력해주세요")
+age=int(input("태어난 년도를 입력해주세요"))
+print("저의 이름은",name,"이고, 올해 나이는",2023-age+1,"세 입니다.")
+
+first=input("첫 번째 문장을 입력해주세요 ")
+second=input("두 번째 문장을 입력해주세요 ")
+print(first+second)
+
+num = int(input("숫자를 입력해주세요"))
+print(-num)
+
+a = int(input("첫 번째 숫자를 입력해주세요"))
+b = int(input("두 번째 숫자를 입력해주세요"))
+print("더하기 연산 : ", a+b)
+print("빼기 연산 : ", a-b)
+print("곱하기 연산 : ", a*b)
+print("몫 연산 : ", a//b)
+print("나머지 연산 : ", a%b)
+
+a=int(input("첫 번째 숫자를 입력해주세요"))
+b=int(input("두 번째 숫자를 입력해주세요"))
+c=int(input("세 번째 숫자를 입력해주세요"))
+print((a+b+c)//3)
+
+a=int(input("첫 번째 숫자를 입력해주세요"))
+b=int(input("두 번째 숫자를 입력해주세요"))
+c=int(input("세 번째 숫자를 입력해주세요"))
+d=int(input("네 번째 숫자를 입력해주세요"))
+e=int(input("다섯 번째 숫자를 입력해주세요"))
+num= [a,b,c,e,d]
+print(num)
+
+a=input("문자열을 입력해주세요")
+b=int(input("숫자를 입력해주세요"))
+print(a*3)
+
+a=int(input("첫 번째 숫자를 입력해주세요"))
+print(a)
+b=a+int(input("두 번째 숫자를 입력해주세요"))
+print(b)
+c=b+int(input("세 번째 숫자를 입력해주세요"))
+print(c)
+d=c+int(input("네 번째 숫자를 입력해주세요"))
+print(d)
+e=d+int(input("다섯 번째 숫자를 입력해주세요"))
+print(e)
