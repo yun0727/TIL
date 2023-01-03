322bae8c252ff216bea3c6a3863e213bb462f321
yun <austru00@gmail.com>
2023-01-03 15:21:07 +0900

    230103

---
+++ 2python/실습2.py
@@ -0,0 +1,45 @@
+# A = int(input("정수를 입력하세요"))
+# if A>0:
+#     print(True)
+# else:
+#     print(False)
+
+# A=int(input("첫 번째 정수를 입력하세요"))
+# B=int(input("두 번째 정수를 입력하세요"))
+# if A>B:
+#     print(A)
+# elif A<B:
+#     print(B)
+# else:
+#     print(False)
+
+# A = int(input("정수를 입력하세요"))
+# if (A>1 and A<10):
+#     print(True)
+# else:
+#     print(False)
+
+# A=int(input("정수를 입력하세요"))
+# if (A>0 and A%2==0):
+#     print(True)
+# else:
+#     print(False)
+
+# A=int(input("정수를 입력하세요"))
+# if (A>100 or A<0):
+#     print("에러")
+# elif(A>=60):
+#     print("합격")
+# else:
+#     print("불합격")
+    
+# word=input("문자열을 입력하세요")
+# for char in word[::-1]:
+#     print(char)
+
+A=int(input("첫 번째 정수를 입력하세요"))
+B=int(input("두 번째 정수를 입력하세요"))
+
+c=[A,B]
+for num in c:
+    print(num)
