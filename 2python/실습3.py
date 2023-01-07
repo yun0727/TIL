58acc8c5d465e8f47e5b07b680a1358ca0b49e24
yun <austru00@gmail.com>
2023-01-07 15:20:05 +0900

    230104S

--- 2python/실습3.py
+++ 2python/실습3.py
@@ -1,3 +1,4 @@
+#230104 제어문(2)
 #문제1 정수 값 입력받고 절대값 출력
 a=int(input("정수를 입력하세요"))
 if a<0:
@@ -24,10 +25,9 @@ print(sum)
 #문제4 리스트의 값 입력받고 리스트에 저장된 정수 평균 출력
 number_list=list(input("number_list = ").split())
 sum=0
-for a in number_list:
-    sum = sum+int(a)
 b=0
 for a in number_list:
+    sum = sum+int(a)
     b=b+1
 print(float(sum/b))
 
