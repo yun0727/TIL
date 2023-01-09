8c24b8e3e7dc9e681b726316d1659efb6ff4e2d8
yun <austru00@gmail.com>
2023-01-09 13:46:06 +0900

    230109

---
+++ 2python/실습5.py
@@ -0,0 +1,37 @@
+#입출력문제 230109
+
+# #1 5를 입력 받아 보세요
+# num=int(input())
+# print(num)
+
+# #2 2 5를 입력받아서 출력하세요
+# n1, n2=map(int,input().split())
+# print(n1,n2)
+
+# #3 1 2 을 입력받아서 출력하세요
+# numbers=list(map(int,input().split()))
+# print(numbers)
+
+# #4 word1 word2 word3을 입력받아서 출력하세요
+# str1, str2, str3=input().split()
+# print(str1, str2, str3)
+
+# #5 1 2 3 4 5를 입력받아서 출력하세요
+# num=list(map(int,input().split()))
+# print(num)
+
+# #6 -10 10을 입력받아서 출력하세요
+# a,b=map(int,input().split())
+# print(a,b)
+
+# #7 a b c d e를 입력받아서 출력하세요
+# str=list(input().split())
+# print(str)
+
+# #8 3 17 1 39 8 41 2 32 99 2를 입력받아서 출력하세요
+# num=list(map(int,input().split()))
+# print(num)
+
+#9 1 4 0 10 2 3 9를 입력받아서 출력하세요
+num=list(map(int,input().split()))
+print(num)
